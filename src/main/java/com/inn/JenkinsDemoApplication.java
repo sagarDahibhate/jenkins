8ec97b96e7091444463ac17c111153bbd69a8b53
sagarDahibhate <55349051+sagarDahibhate@users.noreply.github.com>
2021-04-28 08:27:42 -0700

    Update JenkinsDemoApplication.java
    firstcomm

--- src/main/java/com/inn/JenkinsDemoApplication.java
+++ src/main/java/com/inn/JenkinsDemoApplication.java
@@ -8,7 +8,7 @@ public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
-		System.out.println("hello world in jenkins sagar");
+		System.out.println("hello world in jenkins sagar dahibhate");
 	}
 
 }
