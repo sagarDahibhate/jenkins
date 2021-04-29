88328046066a5cb892b7d6006bb7a778d8b2bb45
unknown <sagardahibhate1@gmail.com>
2021-04-28 19:02:28 -0700

    initial commit

---
+++ src/main/java/com/inn/JenkinsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.inn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+		System.out.println("hello world in jenkins sagar");
+	}
+
+}
